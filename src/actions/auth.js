a0058c1e4ffc07fc64964265cedcc20adbdf6ac4
Ezequiel <zapata.ed1989@gmail.com>
2022-01-05 04:29:13 -0300

    v1 calendarApp

---
+++ src/actions/auth.js
@@ -0,0 +1,102 @@
+import Swal from "sweetalert2";
+import { fetchConToken, fetchSinToken } from "../helpers/fetch"
+import { types } from "../types/types";
+import { eventClearLogout } from "./events";
+
+
+
+export const startLogin = ( email, password ) =>{
+    return async( dispatch ) => {
+
+        const resp = await( fetchSinToken( 'auth', { email, password}, 'POST' ) );
+        const body = await resp.json();
+
+        if( body.ok ) {
+            localStorage.setItem( 'token', body.token )
+            localStorage.setItem( 'token-init-date', new Date().getTime() )
+
+
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+                })
+            );
+        } else {
+
+            Swal.fire( 'error', body.msg, 'error' )
+
+        };
+    }
+
+};
+
+export const  startRegister = ( email , password , name ) =>{
+    return async( dispatch ) =>{
+
+        const resp = await( fetchSinToken( 'auth/new', {  email, password, name}, 'POST' ) );
+        const body = await resp.json();
+
+        if( body.ok ){
+            localStorage.setItem( 'token', body.token )
+            localStorage.setItem( 'token-init-date', new Date().getTime() )
+
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+                })
+            );
+        }else {
+
+            Swal.fire( 'Error', body.msg, 'error' )
+
+        };
+
+    }
+
+};
+
+export const startChecking = () =>{
+    return async(dispatch)=>{
+                          // fetchConToken ('auth/renew', {  }, 'GET' ) ---> no necesita argumentos y por defecto es get                 
+        const resp = await( fetchConToken( 'auth/renew' ) );
+        const body = await resp.json();
+
+        if( body.ok ){
+            localStorage.setItem( 'token', body.token )
+            localStorage.setItem( 'token-init-date', new Date().getTime() )
+
+            dispatch( login({
+                uid: body.uid,
+                name: body.name
+                })
+            );
+        }else {
+            dispatch( checkingFinish() )
+        };
+
+
+
+
+    }
+}
+
+const checkingFinish = () => ({ type: types.authChekingFinish })
+
+const login = ( user ) => ({
+    type: types.authLogin,
+    payload: user
+
+});
+
+export const startLogout = () => {
+    return ( dispatch )=>{
+
+        localStorage.clear();
+        dispatch(eventClearLogout())
+        dispatch( logout() )
+    }
+} 
+
+const logout = () => ({ type: types.authLogOut })
+
+
---
+++ src/types/types.js
@@ -0,0 +1,26 @@
+
+export const types = {
+
+    uiOpenModal: '[ui] Open modal',
+    uiCloseModal: '[ui] Close modal',
+
+    eventSetActive: '[event] Set Active',
+    eventLogout: '[event]  event logout',
+    
+    eventStartAddNew: '[event] Start add new',
+    eventAddNew: '[event]  Add new',
+    eventClearActiveEvent: '[event]  Clear active event',
+    eventUpdated: '[event]  Clear updated',
+    eventDeleted: '[event]  Clear deleted',
+    eventLoaded: '[event] Events Loaded ',
+
+
+    authChekingFinish: '[auth] Finish Cheking login state ',
+    authStartLogin: '[auth] Start Login',
+    authLogin: '[auth] Login',
+    authStartRegister: '[auth] Start Register',
+    authStartTokenRenew: '[auth] Start token renew',
+    authLogOut: '[auth] LogOut',
+    
+
+}
